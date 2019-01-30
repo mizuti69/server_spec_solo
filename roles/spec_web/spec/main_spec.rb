2407b29c5ac42e1f7271717291ada4b72d0b4233
wakabayashi.t <mizuti7802@gmail.com]>
2019-01-30 17:24:46 +0900

    update from spec_apache to spec_web

--- roles/spec_web/spec/main_spec.rb
+++ roles/spec_web/spec/main_spec.rb
@@ -1,34 +1,38 @@
 require 'spec_helper'
 
-describe 'apache setting' do
-  describe package('httpd') do
-    it { should be_installed }
-    it { should be_installed.with_version("#{property['apache_version']}") }
-    describe service('httpd') do
-      it { should be_enabled }
-      it { should be_running }
-      # active only test (not good)
-      status = Specinfra.backend.run_command("systemctl status httpd |awk '/Active/{print $2}'")
-      if status.stdout == "active\n"
-        describe command("httpd -V") do
-          its(:stdout) { should contain("#{property['apache_mpm']}").after('Server MPM') }
-        end
-        # include confs
-        property['apache_include_conf'].each do |configs|
-          describe file("#{configs}") do
-            it { should exist }
+# apache check
+status = Specinfra.backend.run_command("httpd -v")
+if status.exit_status == 0
+  describe 'apache setting' do
+    describe package('httpd') do
+      it { should be_installed }
+      it { should be_installed.with_version("#{property['apache_version']}") }
+      describe service('httpd') do
+        it { should be_enabled }
+        it { should be_running }
+        # active only test (not good)
+        status = Specinfra.backend.run_command("systemctl status httpd |awk '/Active/{print $2}'")
+        if status.stdout == "active\n"
+          describe command("httpd -V") do
+            its(:stdout) { should contain("#{property['apache_mpm']}").after('Server MPM') }
           end
-        end
-        property['apache_listen_ports'].each do |ports|
-          describe port("#{ports}") do
-            it { should be_listening.with('tcp') }
+          # include confs
+          property['apache_include_conf'].each do |configs|
+            describe file("#{configs}") do
+              it { should exist }
+            end
+          end
+          property['apache_listen_ports'].each do |ports|
+            describe port("#{ports}") do
+              it { should be_listening.with('tcp') }
+            end
           end
         end
       end
-    end
-    property['apache_sub_packages'].each do |packages|
-      describe package("#{packages}") do
-        it { should be_installed }
+      property['apache_sub_packages'].each do |packages|
+        describe package("#{packages}") do
+          it { should be_installed }
+        end
       end
     end
   end
